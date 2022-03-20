694fdf74838249e83165adad8fca5b247a559a99
ehmicky <ehmicky@gmail.com>
2022-03-20 19:49:21 +0100

    Add more tests

--- test/set.js
+++ test/set.js
@@ -4,7 +4,22 @@ import { set } from 'wild-wild-path'
 
 each(
   [true, false],
-  [{ target: { one: 1 }, query: 'one', value: 2, output: { one: 2 } }],
+  [
+    { target: { one: 1 }, query: 'one', value: 2, output: { one: 2 } },
+    { target: { one: 1 }, query: 'two', value: 2, output: { one: 1, two: 2 } },
+    {
+      target: { one: 1 },
+      query: 'two three',
+      value: 2,
+      output: { one: 1, two: 2, three: 2 },
+    },
+    {
+      target: { one: {} },
+      query: 'one.two',
+      value: 2,
+      output: { one: { two: 2 } },
+    },
+  ],
   ({ title }, mutate, { target, query, value, opts, output }) => {
     test(`set() output | ${title}`, (t) => {
       t.deepEqual(set(target, query, value, { mutate, ...opts }), output)
