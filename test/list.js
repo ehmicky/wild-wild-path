8b5e444b985833a0a149d12bbe62b57bcfad6889
ehmicky <ehmicky@gmail.com>
2022-03-20 19:43:35 +0100

    Add more tests

--- test/list.js
+++ test/list.js
@@ -214,6 +214,9 @@ each(
     },
 
     // RegExp tokens
+    { target: { one: 1, two: 2, three: 3 }, query: '/t/', output: [2, 3] },
+    { target: { one: 1, two: 2, three: 3 }, query: '/T/', output: [] },
+    { target: { one: 1, two: 2, three: 3 }, query: '/T/i', output: [2, 3] },
     {
       target: { one: 1 },
       query: '/a/',
