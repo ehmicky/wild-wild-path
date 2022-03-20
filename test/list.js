e908372685448e2893be91f5e5e2ec8d8fc09f20
ehmicky <ehmicky@gmail.com>
2022-03-20 19:45:29 +0100

    Split file

--- test/list.js
+++ test/list.js
@@ -251,24 +251,6 @@ each(
       output: [],
       opts: { missing: true, entries: true },
     },
-
-    // RegExp tokens
-    { target: { one: 1, two: 2, three: 3 }, query: '/t/', output: [2, 3] },
-    { target: { one: 1, two: 2, three: 3 }, query: [/t/u], output: [2, 3] },
-    { target: { one: 1, two: 2, three: 3 }, query: '/T/', output: [] },
-    { target: { one: 1, two: 2, three: 3 }, query: '/T/i', output: [2, 3] },
-    {
-      target: { one: 1 },
-      query: '/a/',
-      output: [],
-      opts: { missing: true, entries: true },
-    },
-    {
-      target: [],
-      query: '/a/',
-      output: [],
-      opts: { missing: true, entries: true },
-    },
   ],
   ({ title }, list, { target, query, opts, output }) => {
     test(`list() output | ${title}`, (t) => {
---
+++ test/tokens/regexp.js
@@ -0,0 +1,31 @@
+import test from 'ava'
+import { each } from 'test-each'
+
+import { listMethods } from '../helpers/list.js'
+
+each(
+  listMethods,
+  [
+    { target: { one: 1, two: 2, three: 3 }, query: '/t/', output: [2, 3] },
+    { target: { one: 1, two: 2, three: 3 }, query: [/t/u], output: [2, 3] },
+    { target: { one: 1, two: 2, three: 3 }, query: '/T/', output: [] },
+    { target: { one: 1, two: 2, three: 3 }, query: '/T/i', output: [2, 3] },
+    {
+      target: { one: 1 },
+      query: '/a/',
+      output: [],
+      opts: { missing: true, entries: true },
+    },
+    {
+      target: [],
+      query: '/a/',
+      output: [],
+      opts: { missing: true, entries: true },
+    },
+  ],
+  ({ title }, list, { target, query, opts, output }) => {
+    test(`list() output | ${title}`, (t) => {
+      t.deepEqual(list(target, query, opts), output)
+    })
+  },
+)
