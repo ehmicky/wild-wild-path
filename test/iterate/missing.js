32967aa240779f7155e9b01e8879876ee4933521
ehmicky <ehmicky@gmail.com>
2022-03-20 19:45:59 +0100

    Add tests

---
+++ test/iterate/missing.js
@@ -0,0 +1,43 @@
+import test from 'ava'
+import { each } from 'test-each'
+
+import { listMethods } from '../helpers/list.js'
+
+each(
+  listMethods,
+  [
+    {
+      target: { one: 1 },
+      query: 'one two',
+      output: [
+        { value: 1, path: ['one'], missing: false },
+        { value: undefined, path: ['two'], missing: true },
+      ],
+      opts: { missing: true, entries: true },
+    },
+    {
+      target: {},
+      query: 'one',
+      output: [{ value: undefined, path: ['one'], missing: true }],
+      opts: { missing: true, entries: true },
+    },
+    { target: { one: undefined }, query: 'one', output: [undefined] },
+    {
+      target: { one: undefined },
+      query: 'one',
+      output: [{ value: undefined, path: ['one'], missing: false }],
+      opts: { missing: true, entries: true },
+    },
+    {
+      target: [],
+      query: 'one',
+      output: [{ value: undefined, path: ['one'], missing: true }],
+      opts: { missing: true, entries: true },
+    },
+  ],
+  ({ title }, list, { target, query, opts, output }) => {
+    test(`list() output | ${title}`, (t) => {
+      t.deepEqual(list(target, query, opts), output)
+    })
+  },
+)
--- test/list.js
+++ test/list.js
@@ -115,36 +115,6 @@ each(
       opts: { sort: true },
     },
 
-    // `missing` option
-    {
-      target: { one: 1 },
-      query: 'one two',
-      output: [
-        { value: 1, path: ['one'], missing: false },
-        { value: undefined, path: ['two'], missing: true },
-      ],
-      opts: { missing: true, entries: true },
-    },
-    {
-      target: {},
-      query: 'one',
-      output: [{ value: undefined, path: ['one'], missing: true }],
-      opts: { missing: true, entries: true },
-    },
-    { target: { one: undefined }, query: 'one', output: [undefined] },
-    {
-      target: { one: undefined },
-      query: 'one',
-      output: [{ value: undefined, path: ['one'], missing: false }],
-      opts: { missing: true, entries: true },
-    },
-    {
-      target: [],
-      query: 'one',
-      output: [{ value: undefined, path: ['one'], missing: true }],
-      opts: { missing: true, entries: true },
-    },
-
     // `classes` and `inherited` options
     {
       target: child,
