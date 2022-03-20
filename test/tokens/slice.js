557028d59a704bc983157e891269d130e2ef4d1b
ehmicky <ehmicky@gmail.com>
2022-03-20 21:23:08 +0100

    Fix code duplication

--- test/tokens/slice.js
+++ test/tokens/slice.js
@@ -3,6 +3,8 @@ import { each } from 'test-each'
 
 import { listMethods } from '../helpers/list.js'
 
+const missingOpts = { missing: true, entries: true }
+
 each(
   listMethods,
   [
@@ -15,30 +17,15 @@ each(
     { target: [0, 1, 2, 3], query: '1:-1', output: [1, 2] },
     { target: [0, 1, 2, 3], query: '-3:3', output: [1, 2] },
     { target: [0, 1, 2, 3], query: '-5:3', output: [0, 1, 2] },
-    {
-      target: [0, 1, 2, 3],
-      query: '-6:-5',
-      output: [],
-      opts: { missing: true, entries: true },
-    },
+    { target: [0, 1, 2, 3], query: '-6:-5', output: [], opts: missingOpts },
     { target: [0, 1, 2, 3], query: '1:5', output: [1, 2, 3] },
     { target: [0, 1, 2, 3], query: '1:-0', output: [1, 2, 3] },
-    {
-      target: [0, 1, 2, 3],
-      query: '5:6',
-      output: [],
-      opts: { missing: true, entries: true },
-    },
+    { target: [0, 1, 2, 3], query: '5:6', output: [], opts: missingOpts },
     { target: [0, 1, 2, 3], query: '1:', output: [1, 2, 3] },
     { target: [0, 1, 2, 3], query: ':3', output: [0, 1, 2] },
     { target: [0, 1, 2, 3], query: ':', output: [0, 1, 2, 3] },
     { target: [], query: '0:5', output: [] },
-    {
-      target: {},
-      query: ':',
-      output: [],
-      opts: { missing: true, entries: true },
-    },
+    { target: {}, query: ':', output: [], opts: missingOpts },
   ],
   ({ title }, list, { target, query, opts, output }) => {
     test(`list|iterate() output | ${title}`, (t) => {
