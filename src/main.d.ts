652816547e23811f8088f77d48000eeb306e0b4e
ehmicky <ehmicky@gmail.com>
2022-03-27 19:50:21 +0200

    Add more types

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,1 +1,21 @@
-export default function example(): void
+import type { Query } from 'wild-wild-parser'
+
+export type Target = object | any[]
+export type { Query }
+export interface Options {
+  readonly childFirst?: boolean
+  readonly roots?: boolean
+  readonly leaves?: boolean
+  readonly sort?: boolean
+  readonly missing?: boolean
+  readonly entries?: boolean
+  readonly classes?: boolean
+  readonly inherited?: boolean
+}
+
+export function has(target: Target, query: Query, options?: Options): boolean
+export function get(
+  target: Target,
+  query: Query,
+  options?: Options,
+): any | undefined
--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,4 +1,31 @@
-import wildWildPath from 'wild-wild-path'
+import { has, get, Target, Query, Options } from 'wild-wild-path'
 import { expectType, expectError } from 'tsd'
 
-await wildWildPath()
+const targetObj: Target = { one: 1 }
+const targetArray: Target = [1]
+const targetSpecialObj: Target = /regexp/
+const queryArray: Query = [{ type: 'any' }]
+const queryString: Query = '*'
+const optionsMin: Options = {}
+const optionsFull: Options = {
+  childFirst: false,
+  roots: false,
+  leaves: false,
+  sort: false,
+  missing: false,
+  entries: false,
+  classes: false,
+  inherited: false,
+}
+
+expectType<boolean>(has({}, 'prop'))
+has({}, 'prop', { sort: true })
+expectError(has(true, 'prop'))
+expectError(has({}, true))
+expectError(has({}, 'prop', true))
+
+expectType<any>(get({}, 'prop'))
+get({}, 'prop', { sort: true })
+expectError(get(true, 'prop'))
+expectError(get({}, true))
+expectError(get({}, 'prop', true))
