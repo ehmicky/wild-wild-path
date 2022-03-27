60d34f8aaf0eaffc3780968d37c73f715c637ba8
ehmicky <ehmicky@gmail.com>
2022-03-27 23:54:20 +0200

    Move lines

--- test/helpers/inherited.js
+++ test/helpers/inherited.js
@@ -2,33 +2,7 @@
 import test from 'ava'
 import { each } from 'test-each'
 
-// Retrieve objects with both:
-//  - Enumerated and non-enumerated properties
-//  - Own and inherited properties
-export const getChild = function () {
-  return new Child()
-}
-
-/* eslint-disable fp/no-class, fp/no-this, fp/no-mutation,
-   fp/no-mutating-methods */
-class Parent {}
-
-Parent.prototype.inheritedEnum = 'inheritedEnum'
-Object.defineProperty(Parent.prototype, 'inheritedNonEnum', {
-  value: 'inheritedNonEnum',
-})
-
-class Child extends Parent {
-  constructor() {
-    super()
-    this.ownEnum = 'ownEnum'
-    Object.defineProperty(this, 'ownNonEnum', { value: 'ownNonEnum' })
-  }
-}
-/* eslint-enable fp/no-class, fp/no-this, fp/no-mutation,
-   fp/no-mutating-methods */
-
-// Test those properties
+// Test the `classes` and `inherited` options
 export const testInherited = function (methods, inputs) {
   each(methods, inputs, testInheritedSingle)
 }
@@ -56,3 +30,29 @@ const testInheritedSingle = function (
     )
   })
 }
+
+// Retrieve objects with both:
+//  - Enumerated and non-enumerated properties
+//  - Own and inherited properties
+export const getChild = function () {
+  return new Child()
+}
+
+/* eslint-disable fp/no-class, fp/no-this, fp/no-mutation,
+   fp/no-mutating-methods */
+class Parent {}
+
+Parent.prototype.inheritedEnum = 'inheritedEnum'
+Object.defineProperty(Parent.prototype, 'inheritedNonEnum', {
+  value: 'inheritedNonEnum',
+})
+
+class Child extends Parent {
+  constructor() {
+    super()
+    this.ownEnum = 'ownEnum'
+    Object.defineProperty(this, 'ownNonEnum', { value: 'ownNonEnum' })
+  }
+}
+/* eslint-enable fp/no-class, fp/no-this, fp/no-mutation,
+   fp/no-mutating-methods */
