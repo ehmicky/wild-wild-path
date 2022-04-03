5522d2756c9cf27a6a50337a4db3290d5e325859
ehmicky <ehmicky@gmail.com>
2022-04-03 13:43:50 +0200

    Start optimized path

--- src/iterate/path.js
+++ src/iterate/path.js
@@ -1,6 +1,54 @@
-import { iterateQuery } from './query.js'
+import { iterateChildEntries } from './path_children.js'
 
 // `iterate()` logic when the query is path
 export const iteratePath = function* (target, pathArray, opts) {
-  yield* iterateQuery(target, [pathArray], opts)
+  const entries = getRootEntries(target, pathArray)
+  yield* iterateLevel(entries, 0, opts)
+}
+
+const getRootEntries = function (target, pathArray) {
+  return [{ queryArray: pathArray, value: target, path: [], missing: false }]
+}
+
+// The `roots` option can be used to only include the highest ancestors.
+// The `leaves` option can be used to only include the lowest descendants.
+// Neither option includes the values in-between.
+const iterateLevel = function* (entries, index, opts) {
+  const parentEntry = getParentEntry(entries, index)
+
+  if (shouldYieldParentFirst(parentEntry, opts)) {
+    yield normalizeEntry(parentEntry, opts)
+  }
+
+  const hasChildren = yield* iterateChildEntries({
+    entries,
+    parentEntry,
+    index,
+    opts,
+    iterateLevel,
+  })
+
+  if (shouldYieldParentLast(parentEntry, hasChildren, opts)) {
+    yield normalizeEntry(parentEntry, opts)
+  }
+}
+
+const getParentEntry = function (entries, index) {
+  return entries.find(({ queryArray }) => queryArray.length === index)
+}
+
+const normalizeEntry = function ({ value, path, missing }, { entries }) {
+  return entries ? { value, path, missing } : value
+}
+
+const shouldYieldParentFirst = function (parentEntry, { childFirst }) {
+  return parentEntry !== undefined && !childFirst
+}
+
+const shouldYieldParentLast = function (
+  parentEntry,
+  hasChildren,
+  { childFirst, leaves },
+) {
+  return parentEntry !== undefined && childFirst && !(leaves && hasChildren)
 }
