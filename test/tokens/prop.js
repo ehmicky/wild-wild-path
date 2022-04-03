bdcc9ebe52b5efd3a20ced66d443d4f7da916e6d
ehmicky <ehmicky@gmail.com>
2022-04-03 13:49:44 +0200

    Add one test

--- test/tokens/prop.js
+++ test/tokens/prop.js
@@ -3,4 +3,5 @@ import { testListOutput } from '../helpers/list.js'
 testListOutput([
   { input: [{ one: 1 }, 'one'], output: [1] },
   { input: [{ one: 1 }, ['one']], output: [1] },
+  { input: [[1], ['0']], output: [1] },
 ])
