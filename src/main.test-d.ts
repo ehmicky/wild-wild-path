4ccfd6e20494d0f61cba6c02700e971c72da4149
ehmicky <ehmicky@gmail.com>
2022-12-04 20:19:28 +0100

    Update type tests

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,4 +1,4 @@
-import { expectType, expectNotType, expectError } from 'tsd'
+import { expectType, expectNotType } from 'tsd'
 
 import {
   has,
@@ -35,25 +35,34 @@ const entry: Entry = { value: 1, path: ['prop'], missing: false }
 
 expectType<boolean>(has({}, 'prop'))
 has({}, 'prop', { sort: true })
-expectError(has(true, 'prop'))
-expectError(has({}, true))
-expectError(has({}, 'prop', true))
+// @ts-expect-error
+has(true, 'prop')
+// @ts-expect-error
+has({}, true)
+// @ts-expect-error
+has({}, 'prop', true)
 
 expectType<any | undefined>(get({}, 'prop'))
 expectType<Entry | undefined>(get({}, 'prop', { entries: true }))
 expectNotType<Entry | undefined>(get({}, 'prop', { entries: false }))
 expectNotType<Entry | undefined>(get({}, 'prop', { sort: true }))
-expectError(get(true, 'prop'))
-expectError(get({}, true))
-expectError(get({}, 'prop', true))
+// @ts-expect-error
+get(true, 'prop')
+// @ts-expect-error
+get({}, true)
+// @ts-expect-error
+get({}, 'prop', true)
 
 expectType<any[]>(list({}, 'prop'))
 expectType<Entry[]>(list({}, 'prop', { entries: true }))
 expectNotType<Entry[]>(list({}, 'prop', { entries: false }))
 expectNotType<Entry[]>(list({}, 'prop', { sort: true }))
-expectError(list(true, 'prop'))
-expectError(list({}, true))
-expectError(list({}, 'prop', true))
+// @ts-expect-error
+list(true, 'prop')
+// @ts-expect-error
+list({}, true)
+// @ts-expect-error
+list({}, 'prop', true)
 
 expectType<Generator<any>>(iterate({}, 'prop'))
 for (const entry of iterate({}, 'prop', { entries: true })) {
@@ -65,21 +74,31 @@ for (const entry of iterate({}, 'prop', { entries: false })) {
 for (const entry of iterate({}, 'prop', { sort: true })) {
   expectNotType<Entry>(entry)
 }
-expectError(iterate(true, 'prop'))
-expectError(iterate({}, true))
-expectError(iterate({}, 'prop', true))
+// @ts-expect-error
+iterate(true, 'prop')
+// @ts-expect-error
+iterate({}, true)
+// @ts-expect-error
+iterate({}, 'prop', true)
 
 expectType<Target>(remove({}, 'prop'))
 remove({}, 'prop', { mutate: true })
-expectError(remove(true, 'prop'))
-expectError(remove({}, true))
-expectError(remove({}, 'prop', true))
+// @ts-expect-error
+remove(true, 'prop')
+// @ts-expect-error
+remove({}, true)
+// @ts-expect-error
+remove({}, 'prop', true)
 
 expectType<Target>(set({}, 'prop', 1))
 set({}, 'prop', 1, { mutate: true })
-expectError(set(true, 'prop', 1))
-expectError(set({}, true, 1))
-expectError(set({}, 'prop', 1, true))
+// @ts-expect-error
+set(true, 'prop', 1)
+// @ts-expect-error
+set({}, true, 1)
+// @ts-expect-error
+set({}, 'prop', 1, true)
 
 expectType<boolean>(isObject({}, true))
-expectError(isObject({}, {}))
+// @ts-expect-error
+isObject({}, {})
